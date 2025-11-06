4dca151c43146b5509abf1f4d41f0a5e72ebd07b
dbalsk <dbalsrltk@naver.com>
2025-11-06 13:01:29 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/com/Jejumate/Jejumate_BE/JejumateBeApplication.java
@@ -0,0 +1,13 @@
+package com.Jejumate.Jejumate_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejumateBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejumateBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jejumate/Jejumate_BE/JejumateBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jejumate.Jejumate_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejumateBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
