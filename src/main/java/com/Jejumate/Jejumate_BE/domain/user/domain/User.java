59d975fa40b713e545ee3a202e0749953fb170c2
dbalsk <dbalsrltk@naver.com>
2025-11-07 23:02:29 +0900

    feat: User 엔티티 구현

--- src/main/java/com/Jejumate/Jejumate_BE/domain/user/domain/User.java
+++ src/main/java/com/Jejumate/Jejumate_BE/domain/user/domain/User.java
@@ -1,4 +1,42 @@
 package com.Jejumate.Jejumate_BE.domain.user.domain;
 
-public class User {
+import com.Jejumate.Jejumate_BE.domain.user.enums.Provider;
+import com.Jejumate.Jejumate_BE.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Provider provider;
+
+    @Column(name = "provider_id", nullable = false)
+    private String providerId;
+
+    @Builder
+    public User(String email, Provider provider, String providerId) {
+        this.email = email;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
+
+    // ========== 비즈니스 로직 메서드 ==========
 }
