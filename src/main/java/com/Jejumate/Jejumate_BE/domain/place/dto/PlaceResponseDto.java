74cdd4b559fd4b8d4ee95076997afa3ab1e2c263
Hheojiwon <0618amy@naver.com>
2025-11-21 23:53:52 +0900

    feat: 제주도 인기 여행지 top 7 반환 기능

---
+++ src/main/java/com/Jejumate/Jejumate_BE/domain/place/dto/PlaceResponseDto.java
@@ -0,0 +1,29 @@
+package com.Jejumate.Jejumate_BE.domain.place.dto;
+
+import com.Jejumate.Jejumate_BE.domain.place.domain.Place;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceResponseDto {
+    private Long id;
+    private String name;
+    private String category;
+    private String address;
+    private String imageUrl;
+
+    public static PlaceResponseDto from(Place place) {
+        return PlaceResponseDto.builder()
+                .id(place.getId())
+                .name(place.getName())
+                .category(place.getCategory())
+                .address(place.getAddress())
+                .imageUrl(place.getImageUrl())
+                .build();
+    }
+}
