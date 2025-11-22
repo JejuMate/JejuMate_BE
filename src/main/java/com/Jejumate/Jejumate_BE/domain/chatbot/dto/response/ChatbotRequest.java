62e0f7dae83f6ab6fe4d020612091c3d4982e792
dbalsk <dbalsrltk@naver.com>
2025-11-23 02:23:09 +0900

    feat: ChatbotRequest 구현

--- src/main/java/com/Jejumate/Jejumate_BE/domain/chatbot/dto/response/ChatbotRequest.java
+++ src/main/java/com/Jejumate/Jejumate_BE/domain/chatbot/dto/response/ChatbotRequest.java
@@ -1,4 +1,20 @@
 package com.Jejumate.Jejumate_BE.domain.chatbot.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
 public class ChatbotRequest {
+
+    private List<Message> messages;
+
+    @Getter
+    @Builder
+    public static class Message {
+        private String role;
+        private String content;
+    }
 }
