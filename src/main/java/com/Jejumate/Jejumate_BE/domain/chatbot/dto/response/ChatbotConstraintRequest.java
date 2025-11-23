ae5bbf77bf04f2f5ceaa8dede12cac9572a08d51
dbalsk <dbalsrltk@naver.com>
2025-11-23 13:31:12 +0900

    feat: 프론트엔트 입력받는 ChatbotRequest 구현

--- src/main/java/com/Jejumate/Jejumate_BE/domain/chatbot/dto/response/ChatbotConstraintRequest.java
+++ src/main/java/com/Jejumate/Jejumate_BE/domain/chatbot/dto/response/ChatbotConstraintRequest.java
@@ -1,4 +1,31 @@
 package com.Jejumate.Jejumate_BE.domain.chatbot.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@ToString
 public class ChatbotConstraintRequest {
+
+    @JsonProperty("start_date")
+    private LocalDate startDate;
+
+    @JsonProperty("end_date")
+    private LocalDate endDate;
+
+    @JsonProperty("travel_style")
+    private String travelStyle;
+
+    private String companions;
+
+    @JsonProperty("age_group")
+    private String ageGroup;
+
+    @JsonProperty("additional_request")
+    private String additionalRequest;
 }
--- src/main/java/com/Jejumate/Jejumate_BE/domain/chatbot/dto/response/ChatbotRequest.java
+++ src/main/java/com/Jejumate/Jejumate_BE/domain/chatbot/dto/response/ChatbotRequest.java
@@ -1,12 +1,14 @@
 package com.Jejumate.Jejumate_BE.domain.chatbot.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Getter
 @NoArgsConstructor
 @ToString
-public class ChatbotRequestDto {
+public class ChatbotRequest {
 
     private String action;
 
